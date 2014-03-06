d3d03e1c8d2b61bb2736acb0b7d14dcbe36caac7
just0rz@gmail.com <just0rz@gmail.com@6e18dc39-78ac-79b0-4b7e-131045c9e59a>
2014-03-06 12:37:09 +0000

    Initial Version

---
+++ Projects/WGSharpAPILibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WarGaming API Sharp Library")]
+[assembly: AssemblyDescription("WarGaming API Sharp Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Personal")]
+[assembly: AssemblyProduct("WarGaming API Sharp Library")]
+[assembly: AssemblyCopyright("Copyright © Iulian Grosu 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c01f5297-f1f3-4548-bd18-fba2271357b5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

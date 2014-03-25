a1a771d161593c9c5dfd5833462d231b011cf108
just0rz@gmail.com <just0rz@gmail.com@6e18dc39-78ac-79b0-4b7e-131045c9e59a>
2014-03-25 15:15:16 +0000

    Added a simple sample project

---
+++ Samples/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WGSharpAPI;
+using WGSharpAPI.Interfaces;
+
+namespace SampleWGSharpAPI
+{
+    class Program
+    {
+        /// <summary>
+        /// The application id is provided to you by WarGaming.
+        /// Go to developer's room and create an application.
+        /// You will receive an application id and you will then be able to make calls to WG's API using this library
+        /// </summary>
+        private const string applicationId = "demo"; // you should replace this
+
+        static void Main(string[] args)
+        {
+
+            ShowSearchPlayersSample();
+
+        }
+
+        public static void ShowSearchPlayersSample()
+        {
+            // we initialize the WGApplication object by calling the most basic constructor which needs you to supply your application id
+            IWGApplication wgApplication = new WGApplication(applicationId);
+
+            // The response will be of the WGResponse<T> where T is the type that you will get
+
+            // e.g. calling the SearchPlayers method will return a WGResponse<List<Player>>
+
+            var playersQueryResult = wgApplication.SearchPlayers("Just0rz");
+
+            if (playersQueryResult.Status != "ok")
+            {
+                Console.WriteLine("There was an error when trying to retrieve the items");
+
+                return;
+            }
+
+            if (playersQueryResult.Count <= 0)
+            {
+                Console.WriteLine("Query yielded no results.");
+
+                return;
+            }
+
+            foreach (var player in playersQueryResult.Data)
+            {
+                Console.WriteLine("AccountID: {0}, Nickname: {1}", player.AccountId, player.Nickname);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

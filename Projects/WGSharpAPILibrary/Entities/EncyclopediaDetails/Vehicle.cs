ffe8eaef0c3d0e18756eb048437bc91f2d2a2e11
just0rz@gmail.com <just0rz@gmail.com@6e18dc39-78ac-79b0-4b7e-131045c9e59a>
2014-04-07 13:17:12 +0000

    Added support for two more methods for Encyclopedia - List of vehicles - Vehicle details

---
+++ Projects/WGSharpAPILibrary/Entities/EncyclopediaDetails/Vehicle.cs
@@ -0,0 +1,63 @@
+﻿/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Iulian Grosu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+using Newtonsoft.Json;
+
+namespace WGSharpAPI.Entities.EncyclopediaDetails
+{
+    public class Vehicle
+    {
+        [JsonProperty("is_premium")]
+        public bool IsPremium { get; set; }
+
+        [JsonProperty("level")]
+        public long Tier { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("name_i18n")]
+        public string LocalizedName { get; set; }
+
+        [JsonProperty("nation")]
+        public string Nation { get; set; }
+
+        [JsonProperty("nation_i18n")]
+        public string LocalizedNation { get; set; }
+
+        [JsonProperty("tank_id")]
+        public long Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        #region Overrides
+
+        public override string ToString()
+        {
+            return string.IsNullOrWhiteSpace(LocalizedName) ? base.ToString() : LocalizedName;
+        }
+
+        #endregion Overrides
+    }
+}

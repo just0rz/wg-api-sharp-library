cb15ec842ee034fd3bf39bca16f0eb3d3af35fd1
Iulian Grosu <just0rz@gmail.com>
2015-03-29 00:06:23 +0200

    Fix the way we dealt with statistics by converting the BaseStatistics to abstract and add a StatisticsType enum which must be implemented.
    https://github.com/just0rz/wg-api-sharp-library/issues/3
    
    Found a small issue with Turrets not inheriting from TankModule.

---
+++ Projects/WGSharpAPILibrary/Enums/StatisticsType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WGSharpAPI.Enums
+{
+    public enum StatisticsType
+    {
+        Overall,
+        Clan,
+        Team,
+        Company,
+    }
+}
